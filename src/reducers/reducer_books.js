81b723303c18f59f73c2daa9312b5246798f44e1
Jason Hanna <jason.hanna01@gmail.com>
2016-11-30 14:37:50 -0700

    add working active book selector

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript'},
-    {title: 'Ruby'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'}
+    {title: 'Javascript', pages: 1001},
+    {title: 'Ruby', pages: 2103},
+    {title: 'Harry Potter', pages: 301},
+    {title: 'The Dark Tower', pages: 540}
   ];
 }
